8c1138514b4029fadfe726e27cd98b6c23b30b7d
“Artur <denisov.cloudstorage.it@gmail.com>
2021-07-11 22:01:43 +0300

    Feature #4

--- Git/Program.cs
+++ Git/Program.cs
@@ -12,6 +12,7 @@ namespace Git
             Console.WriteLine("Feature 1");
             Console.WriteLine("Feature 2");
             Console.WriteLine("Feature 3");
+            Console.WriteLine("Feature 4");
             Console.WriteLine("Develop 2");
             Console.WriteLine("Master 2");
         }
